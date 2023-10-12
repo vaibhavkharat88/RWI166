7c4494e3eccd5585140235da33fc1a87824d9f22
vaibhav kharat <vaibhavkharat88@gmail.com>
2023-10-12 18:45:07 +0530

    Constructor_Overloading_Peactise_12_10_23
    Understand the concept of constructor overloading

---
+++ Daily_task/Constructor_overloading.java
@@ -0,0 +1,47 @@
+class Rectangle{
+    private int length; 
+    private int breadth; 
+    public Rectangle(){
+        this.length=5;
+        this.breadth=4;
+    }
+    public Rectangle(int length,int breadth){
+        this.length=length;
+        this.breadth=breadth;
+    }
+    public int getlength(){
+        return length;
+
+    }
+    public int getBreadth(){
+        return breadth;
+        
+    }
+    public int Area(){
+        int value;
+        value=this.length*this.breadth;
+        return value;
+
+    }
+}
+public class Constructor_overloading{
+    public static void main(String[] args) {
+        // default values
+        Rectangle r1=new Rectangle();
+        System.out.println("default constructor");
+        System.out.println(r1.getlength());
+        System.out.println(r1.getBreadth());
+        System.out.println(r1.Area());
+        
+        // constructor with Arguments
+
+        Rectangle r2=new Rectangle(25, 10);
+        System.out.println("constructor with Arguments");
+        System.out.println(r2.getlength());
+        System.out.println(r2.getBreadth());
+        // System.out.println(r2.Area(10,20));
+
+        
+
+    }
+}
